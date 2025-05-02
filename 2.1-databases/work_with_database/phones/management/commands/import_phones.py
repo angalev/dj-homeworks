12e2c924b6b6d1e0ae4ad8f9539a7d0545d16498
angalev <miha-angalev@mail.ru>
2025-05-02 15:16:49 +0300

    2.1 databases

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,8 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
 from phones.models import Phone
 
 
@@ -11,7 +13,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
+            for phone in phones:
+                Phone.objects.create(
+                    name=phone['name'],
+                    price = phone['price'],
+                    image = phone['image'],
+                    release_date = phone['release_date'],
+                    lte_exists = phone['lte_exists'],
+                    slug = slugify(phone['name'])
+                )
+        self.stdout.write(self.style.SUCCESS('Successfully imported phones'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
+
     return redirect('catalog')
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    sorting = request.GET.get('sort')
+
+    if sorting == 'name':
+        phones = Phone.objects.all().order_by('name')
+    if sorting == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    if sorting == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    if not sorting:
+        phones = Phone.objects.all()
+
+    context = {
+        'phones': phones,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone,
+    }
     return render(request, template, context)
